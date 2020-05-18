93285acbf467468e2d6ff10d38e9bb400c8ff971
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2020-05-18 11:22:18 -0400

    refactor: moving virtualization check to virtcrud

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const chalk = require('chalk');
 const child_process = require('child_process')
 const version = require('./package.json').version;
 const virtcrudVersion = JSON.parse(child_process.execSync(`npm list virtcrud --json`, {cwd: __dirname}).toString()).dependencies.virtcrud.resolved.split('#')[1].substring(0, 7);
-const connector = require('infra.connectors').getConnector('local');
-const os = require('os');
 
 // Environment reset/sanity check
 // - prereqs
@@ -18,16 +16,6 @@ const os = require('os');
     if( isWsl )
     {
         console.log(chalk.red(`Error: Running virtualization software inside Windows Subsystem for Linux is not supported.`))
-        return;
-    }
-
-    if (!connector.checkVirt()) {
-        if (os.platform() === 'win32' && connector.checkHyperV())
-            console.log(chalk.red(`Error: VirtualBox VMs cannot run when Hyper-V is enabled.`));
-        
-        else
-            console.log(chalk.red(`Error: Virtualization support is required for running VMs, but it is disabled on this machine.`));
-
         return;
     }
 
