2a247ef5be77bf81d466d66695b70339c4925b40
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2020-06-15 17:56:47 -0400

    test: adding test for bakerx.yml

--- test/test.js
+++ test/test.js
@@ -26,7 +26,7 @@ describe('Run an Alpine 3.9 VM', () => {
         execSync('node index.js pull ottomatica/slim#images alpine3.9-simple', { stdio: 'inherit' });
     });
 
-    test('Run a VM with host-only networking, 1GB memory, and sync folder', () => {
+    test('Run a VM with 1GB memory', () => {
         execSync('node index.js run bakerx-test-vm alpine3.9-simple --memory 1024', { stdio: 'inherit' });
     });
 
@@ -38,5 +38,29 @@ describe('Run an Alpine 3.9 VM', () => {
 
     test('Delete the test VM', () => {
         execSync('node index.js delete vm bakerx-test-vm', { stdio: 'inherit' });
+    });
+})
+
+describe('Run two Alpine 3.9 VMs from bakerx.yml', () => {
+    test('Pull `alpine3.9-simple` Image', () => {
+        execSync('node index.js pull ottomatica/slim#images alpine3.9-simple', { stdio: 'inherit' });
+    });
+
+    test('Run two VMs from bakerx.yml', () => {
+        execSync('cd test/resources && node ../../index.js run', { stdio: 'inherit' });
+    });
+
+    test('Run ssh command on the VM', () => {
+        const vms = ['bakerx-test-vm1', 'bakerx-test-vm2'];
+        for(const vm of vms) {
+            const sshCmd = execSync(`node index.js ssh-info ${vm} | tr -d '"'`).toString().trim().replace('-i', '-qi');
+            const whoami = execSync(`${sshCmd} whoami`).toString().trim();
+            expect(whoami).toBe('root');
+        }
+    });
+
+    test('Delete the test VMs', () => {
+        execSync('node index.js delete vm bakerx-test-vm1', { stdio: 'inherit' });
+        execSync('node index.js delete vm bakerx-test-vm2', { stdio: 'inherit' });
     });
 })
