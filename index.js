1d43fc89571b717872dbe92a544a494214bf2854
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2021-02-01 22:28:16 -0500

    fix: temp fix for `npm list` error on latest npm on linux

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const isWsl = require('is-wsl');
 const chalk = require('chalk');
 const child_process = require('child_process')
 const version = require('./package.json').version;
-const virtcrudVersion = JSON.parse(child_process.execSync(`npm list virtcrud --json`, {cwd: __dirname}).toString()).dependencies.virtcrud.resolved.split('#')[1].substring(0, 7);
+
+let virtcrudVersion;
+try {
+    virtcrudVersion = JSON.parse(child_process.execSync(`npm list virtcrud --json`, { cwd: __dirname }).toString()).dependencies.virtcrud.resolved.split('#')[1].substring(0, 7);
+} catch (err) {
+    virtcrudVersion = 'UNKNOWN';
+}
 
 // Environment reset/sanity check
 // - prereqs
